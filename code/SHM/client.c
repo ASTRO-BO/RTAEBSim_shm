a0fcfcda5d27df3f0867574a13430b88f35e5845
Andrea Zoli <zoli@iasfbo.inaf.it>
2014-03-18 18:40:50 +0100

    Added licenses.

--- code/SHM/client.c
+++ code/SHM/client.c
@@ -1,3 +1,19 @@
+/***************************************************************************
+    copyright            : (C) 2014 Andrea Bulgarelli
+                               2014 Andrea Zoli
+    email                : bulgarelli@iasfbo.inaf.it
+                           zoli@iasfbo.inaf.it
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
--- code/mac_clock_gettime.h
+++ code/mac_clock_gettime.h
@@ -1,3 +1,17 @@
+/***************************************************************************
+    copyright            : (C) 2014 Andrea Bulgarelli
+    email                : bulgarelli@iasfbo.inaf.it
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifdef __MACH__
 #include <sys/time.h>
 #define CLOCK_REALTIME 0
